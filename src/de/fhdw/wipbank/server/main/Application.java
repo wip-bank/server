d736d314780f1ebca466a8b7e32105af58973b83
dsawenko <daniel.sawenko@web.de>
2017-07-22 13:47:14 +0200

    Test commit

--- src/de/fhdw/wipbank/server/main/Application.java
+++ src/de/fhdw/wipbank/server/main/Application.java
@@ -4,7 +4,7 @@ package de.fhdw.wipbank.server.main;
  * Created by pdyck on 20.07.17.
  */
 public class Application {
-	// Test von Daniel
+	// Test von Daniel 1
     public static void main(String[] args) throws Exception {
         JettyServer.run();
     }
