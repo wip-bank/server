b69e0fd9a8f2467428caca14ff93fd7fdb9c4dcd
dsawenko <daniel.sawenko@web.de>
2017-07-23 13:40:04 +0200

    Kommentar as Application.java entfernt

--- src/de/fhdw/wipbank/server/main/Application.java
+++ src/de/fhdw/wipbank/server/main/Application.java
@@ -1,7 +1,6 @@
 package de.fhdw.wipbank.server.main;
 
 public class Application {
-	// Test von Daniel
     public static void main(String[] args) throws Exception {
         JettyServer.run();
     }
