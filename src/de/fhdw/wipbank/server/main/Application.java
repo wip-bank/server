db15526048238d897c3369dfc229d8fac23e9385
dsawenko <daniel.sawenko@web.de>
2017-10-06 11:41:48 +0200

    Kleine Anpassung damit deine Änderung funktioniert

--- src/de/fhdw/wipbank/server/main/Application.java
+++ src/de/fhdw/wipbank/server/main/Application.java
@@ -16,69 +16,72 @@ public class Application {
 
 	private static Logger logger = Logger.getLogger(Application.class);
 
-    /**
-     * Einstieg in das Projekt
-     *
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception {
-        JettyServer.run();
+	/**
+	 * Einstieg in das Projekt
+	 *
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		JettyServer.run();
 
-        initDB(); // Erzeugt Datenbanktabellen, wenn nicht vorhanden.
+		initDB(); // Erzeugt Datenbanktabellen, wenn nicht vorhanden.
 
-        /* Testbetrieb abgeschlossen -> Keine Testdaten mehr notwendig.
-         * Bei weiterer Programmierung können die Testdaten jedoch wieder reaktiviert werden.
-         * initTestData();
-         */
-    }
+		/*
+		 * Testbetrieb abgeschlossen -> Keine Testdaten mehr notwendig. Bei
+		 * weiterer Programmierung können die Testdaten jedoch wieder
+		 * reaktiviert werden. initTestData();
+		 */
+	}
 
-    /**
-     * Datenbanktabellen erzeugen, wenn nicht vorhanden.
-     */
-    private static void initDB(){
-    	AccountService accountService = new AccountService();
-    	TransactionService transactionService = new TransactionService();
-    	try {
-    		logger.info("Datenbank wird initialisiert");
+	/**
+	 * Datenbanktabellen erzeugen, wenn nicht vorhanden.
+	 */
+	private static void initDB() {
+		AccountService accountService = new AccountService();
+		TransactionService transactionService = new TransactionService();
+		try {
+			logger.info("Datenbank wird initialisiert");
 			accountService.createTable();
 			transactionService.createTable();
 			// Konto der Bank vorhanden?
-	        new FindAccountByNumber().findAccountByNumber("0000");
+			Account account = accountService.getAccount("0000");
+			if (account == null) {
+				// Account "0000" nicht vorhanden -> neu erstellen
+				account = new Account();
+				account.setOwner("Bank");
+				account.setNumber("0000");
+				logger.info("Bankkonto wird erstellt: " + account);
+				try {
+					accountService.create(account);
+				} catch (SQLException e1) {
+					logger.error("Konnte Bankkonto nicht erstellen");
+				}
+			}
 
 		} catch (SQLException e) {
 			logger.error("Konnte Tabellen nicht erzeugen");
-		} catch (NotFoundException e) {
-			// Account "0000" nicht vorhanden -> neu erstellen
-			 Account account = new Account();
-	         account.setOwner("Bank");
-	         account.setNumber("0000");
-	         logger.info("Bankkonto wird erstellt: " + account);
-	         try {
-				accountService.create(account);
-			} catch (SQLException e1) {
-				logger.error("Konnte Bankkonto nicht erstellen");
-			}
-        } catch (Exception e) {
+		} catch (Exception e) {
 			logger.error("Fehler initDB");
+			e.printStackTrace();
 		}
-    }
+	}
 
-    /**
-     * Testdaten werden initialisiert
-     */
-    private static void initTestData() {
-        TestDataHelper helper = new TestDataHelper();
-        helper.wipeDatabase();
-        Account bank = helper.createAccount("Bank", "0000");
-        Account daniel = helper.createAccount("Daniel", "1000");
-        Account philipp = helper.createAccount("Philipp", "1001");
-        Account jannis = helper.createAccount("Jannis", "1002");
-        Account alexander = helper.createAccount("Alexander", "1003");
-        helper.createTransaction(bank, daniel, BigDecimal.valueOf(10000.00));
-        helper.createTransaction(bank, philipp, BigDecimal.valueOf(10000.00));
-        helper.createTransaction(bank, jannis, BigDecimal.valueOf(10000.00));
-        helper.createTransaction(bank, alexander, BigDecimal.valueOf(10000.00));
-    }
+	/**
+	 * Testdaten werden initialisiert
+	 */
+	private static void initTestData() {
+		TestDataHelper helper = new TestDataHelper();
+		helper.wipeDatabase();
+		Account bank = helper.createAccount("Bank", "0000");
+		Account daniel = helper.createAccount("Daniel", "1000");
+		Account philipp = helper.createAccount("Philipp", "1001");
+		Account jannis = helper.createAccount("Jannis", "1002");
+		Account alexander = helper.createAccount("Alexander", "1003");
+		helper.createTransaction(bank, daniel, BigDecimal.valueOf(10000.00));
+		helper.createTransaction(bank, philipp, BigDecimal.valueOf(10000.00));
+		helper.createTransaction(bank, jannis, BigDecimal.valueOf(10000.00));
+		helper.createTransaction(bank, alexander, BigDecimal.valueOf(10000.00));
+	}
 
 }
